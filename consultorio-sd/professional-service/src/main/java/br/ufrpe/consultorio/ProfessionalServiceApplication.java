6fe95556eb166cd4a350134834ede9e7a350520e
Flavia Hercilia da Silva <flaviaherciliads@gmail.com>
2025-07-29 08:09:39 -0300

    Correção modulo professional service e patient service

---
+++ consultorio-sd/professional-service/src/main/java/br/ufrpe/consultorio/ProfessionalServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrpe.consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessionalServiceApplication.class, args);
+    }
+
+}
